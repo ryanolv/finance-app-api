07f6ea196d7abb3c90350b2d5abdc47e80816d4e
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-03-18 19:09:41 -0300

    feat: add init migration to create users and transactions tables

---
+++ src/db/postgres/migrations/01-init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS users (
+  ID UUID PRIMARY KEY,
+  firt_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
+        CREATE TYPE transaction_type AS ENUM ('EARNING', 'EXPENSE', 'INVESTMENT');
+    END IF;
+END $$;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id UUID PRIMARY KEY,
+    user_id UUID REFERENCES users(ID) ON DELETE CASCADE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    type transaction_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
