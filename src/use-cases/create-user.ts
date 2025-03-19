2b5a1af251de6c6899b4fb0781218bdd574e860b
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-03-19 13:43:11 -0300

    feat: add GetUserByEmail repository

--- src/use-cases/create-user.ts
+++ src/use-cases/create-user.ts
@@ -5,12 +5,21 @@ import {
   CreateUserParams,
   PostgresCreateUserRepository,
 } from "../repositories/postgres/create-user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
 
 export type CreateUserUseCaseParams = Omit<CreateUserParams, "ID">;
 
 export class CreateUserUseCase {
   async execute(createUserParams: CreateUserUseCaseParams) {
-    // TODO: verify by email if user already exists
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository();
+
+    const userWithProvidedEmail =
+      await postgresGetUserByEmailRepository.execute(createUserParams.email);
+
+    if (userWithProvidedEmail) {
+      throw new Error("User with provided email already exists.");
+    }
 
     const userId = uuidv4();
     const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
