38580d672947a02014943e2cad7d3fe59e3091b9
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-03-19 12:16:51 -0300

    feat: add PostgresGetUserByIdRepository

---
+++ src/repositories/postgres/get-user-by-id.ts
@@ -0,0 +1,11 @@
+import { PostgresHelper } from "../../db/postgres/helper";
+
+export class PostgresGetUserByIdRepository {
+  async execute(userId: string) {
+    const user = await PostgresHelper.query(
+      "SELECT * FROM users WHERE id = $1",
+      [userId],
+    );
+    return user[0];
+  }
+}
