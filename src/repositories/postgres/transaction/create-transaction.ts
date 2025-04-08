23623d2e47f32566be34789a4e0bc3d206252684
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-04-08 12:20:51 -0300

    feat: add PostgresCreateTransactionRepository

---
+++ src/repositories/postgres/transaction/create-transaction.ts
@@ -0,0 +1,31 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js";
+
+interface CreateTransactionParams {
+  id: string;
+  user_id: string;
+  name: string;
+  date: string;
+  amount: string;
+  type: string;
+}
+
+export class PostgresCreateTransactionRepository {
+  async execute(createTransactionParams: CreateTransactionParams) {
+    const createdTransaction = await PostgresHelper.query(
+      `
+      INSERT INTO transactions (id, user_id, name, date, amount, type) 
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING *;
+      `,
+      [
+        createTransactionParams.id,
+        createTransactionParams.user_id,
+        createTransactionParams.name,
+        createTransactionParams.date,
+        createTransactionParams.amount,
+        createTransactionParams.type,
+      ],
+    );
+    return createdTransaction[0];
+  }
+}
