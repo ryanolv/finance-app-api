c1bfcf6345b82b58c5e2a1bb33068fd6932a7111
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-04-08 19:25:04 -0300

    feat: add GetTransactionById factory

--- src/factories/controllers/transaction.ts
+++ src/factories/controllers/transaction.ts
@@ -1,9 +1,12 @@
 import { CreateTransactionController } from "../../controllers/index.js";
+import { GetTransactionByIdController } from "../../controllers/transaction/get-transaction-by-id.js";
 import {
   PostgresCreateTransactionRepository,
   PostgresGetUserByIdRepository,
 } from "../../repositories/postgres/index.js";
+import { PostgresGetTransactionByIdRepository } from "../../repositories/postgres/transaction/get-transaction-by-id.js";
 import { CreateTransactionUseCase } from "../../use-cases/index.js";
+import { GetTransactionByIdUseCase } from "../../use-cases/transaction/get-transaction-by-id.js";
 
 export const makeCreateTransactionController = () => {
   const createTransactionRepository = new PostgresCreateTransactionRepository();
@@ -18,4 +21,16 @@ export const makeCreateTransactionController = () => {
     createTransactionUseCase,
   );
   return createTransactionController;
+};
+
+export const makeGetTransactionByIdController = () => {
+  const getTransactionByIdRepository =
+    new PostgresGetTransactionByIdRepository();
+  const getTransactionByIdUseCase = new GetTransactionByIdUseCase(
+    getTransactionByIdRepository,
+  );
+  const getTransactionByIdController = new GetTransactionByIdController(
+    getTransactionByIdUseCase,
+  );
+  return getTransactionByIdController;
 };
