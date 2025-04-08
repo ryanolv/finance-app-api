8e1d092e709186b377571465ff6502337d8cd73e
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-04-08 18:19:41 -0300

    feat: add PostgresGetTransactionByIdRepository

---
+++ src/repositories/postgres/transaction/get-transaction-by-id.ts
@@ -0,0 +1,11 @@
+import { PostgresHelper } from "../../../db/postgres/helper";
+
+export class PostgresGetTransactionByIdRepository {
+  async execute(transactionId: string) {
+    const transaction = await PostgresHelper.query(
+      `SELECT * FROM transactions WHERE id = $1`,
+      [transactionId],
+    );
+    return transaction[0];
+  }
+}
