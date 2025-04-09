ac630fa560fa2c6a56588357b0a9eea9a4eca184
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-04-09 11:58:29 -0300

    feat: ensure UpdateTransactionUseCase can be acessible in index file

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -5,3 +5,4 @@ export * from "./user/delete-user.js";
 
 export * from "./transaction/create-transaction.js";
 export * from "./transaction/get-transaction-by-id.js";
+export * from "./transaction/update-transaction.js";
